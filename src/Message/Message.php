ba74721171743e2a16e21efa0995c95d9c318ecd
Nicolai Agersbæk <na@smartweb.dk>
2018-07-25 20:43:17 +0200

    Changed MessageInterface structure to represent the raw message format received by subscribers.

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -13,25 +13,70 @@ class Message implements MessageInterface
 {
     
     /**
+     * @var int
+     */
+    private $index;
+    
+    /**
      * @var string
      */
-    private $content;
+    private $subject;
+    
+    /**
+     * @var string
+     */
+    private $data;
+    
+    /**
+     * @var int
+     */
+    private $timestamp;
     
     /**
      * Message constructor.
      *
-     * @param string $content
+     * @param int    $index
+     * @param string $subject
+     * @param string $data
+     * @param int    $timestamp
      */
-    public function __construct(string $content)
+    public function __construct(int $index, string $subject, string $data, int $timestamp)
     {
-        $this->content = $content;
+        $this->index = $index;
+        $this->subject = $subject;
+        $this->data = $data;
+        $this->timestamp = $timestamp;
     }
     
     /**
      * @inheritDoc
      */
-    public function getContent() : string
+    public function getIndex() : int
     {
-        return $this->content;
+        return $this->index;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getSubject() : string
+    {
+        return $this->subject;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getData() : string
+    {
+        return $this->data;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getTimestamp() : int
+    {
+        return $this->timestamp;
     }
 }
--- src/Message/MessageInterface.php
+++ src/Message/MessageInterface.php
@@ -13,9 +13,30 @@ interface MessageInterface
 {
     
     /**
-     * The string representation of the content of this message.
+     * The sequential index of this message.
+     *
+     * @return int
+     */
+    public function getIndex() : int;
+    
+    /**
+     * The subject of this message.
      *
      * @return string
      */
-    public function getContent() : string;
+    public function getSubject() : string;
+    
+    /**
+     * The string representation of the data of this message.
+     *
+     * @return string
+     */
+    public function getData() : string;
+    
+    /**
+     * The timestamp this message was received.
+     *
+     * @return int
+     */
+    public function getTimestamp() : int;
 }
