bf26bfc67586838d33c9b8acde1db46eb3464a67
Nicolai Agersbæk <na@smartweb.dk>
2018-08-09 13:55:12 +0200

    Added use of DateTimeNormalizer and JsonSerializableNormalizer in normalization of payload.

--- src/Payload/Serialization/PayloadNormalizer.php
+++ src/Payload/Serialization/PayloadNormalizer.php
@@ -6,6 +6,8 @@ namespace SmartWeb\Nats\Payload\Serialization;
 
 use SmartWeb\CloudEvents\Nats\Payload\PayloadFields;
 use SmartWeb\CloudEvents\Nats\Payload\PayloadInterface;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
@@ -19,6 +21,11 @@ class PayloadNormalizer implements NormalizerInterface
 {
     
     /**
+     * @var NormalizerInterface[]
+     */
+    private static $fieldNormalizers = [];
+    
+    /**
      * @inheritDoc
      */
     public function normalize($object, $format = null, array $context = null) : array
@@ -27,7 +34,23 @@ class PayloadNormalizer implements NormalizerInterface
             throw new \InvalidArgumentException('The given data is not supported by this normalizer.');
         }
         
-        return $this->convertPayloadToArray($object);
+        return $this->normalizePayload($object);
+    }
+    
+    /**
+     * @param PayloadInterface $payload
+     *
+     * @return array
+     */
+    private function normalizePayload(PayloadInterface $payload) : array
+    {
+        $data = $this->convertPayloadToArray($payload);
+        
+        foreach ($data as $field => &$value) {
+            $value = $this->normalizePayloadField($field, $value);
+        }
+        
+        return $data;
     }
     
     /**
@@ -48,6 +71,50 @@ class PayloadNormalizer implements NormalizerInterface
             PayloadFields::CONTENT_TYPE         => $payload->getContentType(),
             PayloadFields::EXTENSIONS           => $payload->getExtensions(),
             PayloadFields::DATA                 => $payload->getData(),
+        ];
+    }
+    
+    /**
+     * @param string                             $field
+     * @param array|bool|float|int|string|object $value
+     *
+     * @return array|bool|float|int|string|object
+     */
+    private function normalizePayloadField(string $field, $value)
+    {
+        $normalizer = $this->getFieldNormalizer($field);
+        
+        return $normalizer === null
+            ? $value
+            : $normalizer->normalize($value);
+    }
+    
+    /**
+     * @param string $field
+     *
+     * @return null|NormalizerInterface
+     */
+    private function getFieldNormalizer(string $field) : ?NormalizerInterface
+    {
+        return self::getFieldNormalizers()[$field] ?? null;
+    }
+    
+    /**
+     * @return NormalizerInterface[]
+     */
+    private static function getFieldNormalizers() : array
+    {
+        return self::$fieldNormalizers ?? self::$fieldNormalizers = self::resolveFieldNormalizers();
+    }
+    
+    /**
+     * @return NormalizerInterface[]
+     */
+    private static function resolveFieldNormalizers() : array
+    {
+        return [
+            PayloadFields::EVENT_TIME => new DateTimeNormalizer(\DateTime::RFC3339),
+            PayloadFields::DATA       => new JsonSerializableNormalizer(),
         ];
     }
     
